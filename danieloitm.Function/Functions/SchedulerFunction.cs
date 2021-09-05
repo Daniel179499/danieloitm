4f3650548c1bd7c38a1f68ed8f53e121f3644c43
DANIEL ESTEBAN ORTIZ RIOS <danielortiz179499@correo.itm.edu.co>
2021-09-05 11:22:51 -0500

    SchedulerFunction

---
+++ danieloitm.Function/Functions/SchedulerFunction.cs
@@ -0,0 +1,34 @@
+using danieloitm.Function.Entities;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Threading.Tasks;
+
+namespace danieloitm.Function.Functions
+{
+    public static class SchedulerFunction
+    {
+        [FunctionName("SchedulerFunction")]
+        public static async Task Run(
+            [TimerTrigger("0 */10 * * * *")] TimerInfo myTimer,
+                        [Table("todo", Connection = "AzureWebJobsStorage")] CloudTable todoTable,
+            ILogger log)
+        {
+            log.LogInformation($"Deleting completed function executed at: {DateTime.Now}");
+            string filter = TableQuery.GenerateFilterConditionForBool("IsCompleted", QueryComparisons.Equal, true);
+            TableQuery<TodoEntity> query = new TableQuery<TodoEntity>().Where(filter);
+            TableQuerySegment<TodoEntity> completedTodos = await todoTable.ExecuteQuerySegmentedAsync(query, null);
+            int deleted = 0;
+            foreach (TodoEntity completedTodo in completedTodos)
+            {
+                await todoTable.ExecuteAsync(TableOperation.Delete(completedTodo));
+                deleted++;
+            }
+
+            log.LogInformation($"Deleted: {deleted} items ats: {DateTime.Now}");
+
+
+        }
+    }
+}

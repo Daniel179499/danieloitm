42cd992b848dd1d98d9b4783724c4b36db422cba
DANIEL ESTEBAN ORTIZ RIOS <danielortiz179499@correo.itm.edu.co>
2021-09-05 09:16:19 -0500

    Entities

---
+++ danieloitm.Common/Models/Todo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace danieloitm.Common.Models
+{
+    internal class Todo
+    {
+        public DateTime CreatedTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
